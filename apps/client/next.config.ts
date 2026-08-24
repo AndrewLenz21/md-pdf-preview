bfcd6b88d9e296a984451fa618fd183eaa3b58da
AndrewLenz21 <angelitolenz2000@gmail.com>
2026-08-24 11:57:57 +0200

    production url

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["md-pdf-preview.andrew-lenz.com"],
 };
 
 const withNextIntl = createNextIntlPlugin("./src/core/i18n/request.ts");
