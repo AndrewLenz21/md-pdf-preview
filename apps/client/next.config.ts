213183a9a7c3627ab6ec0a92fe6b45e9482839aa
AndrewLenz21 <angelitolenz2000@gmail.com>
2026-08-28 07:24:38 +0200

    pg cloudflare error

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -1,10 +1,19 @@
 import type { NextConfig } from "next";
+
 import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+
   allowedDevOrigins: ["md-pdf-preview.andrew-lenz.com"],
+
+  outputFileTracingIncludes: {
+    "**/*": [
+      "./node_modules/pg-cloudflare/dist/**",
+      "./node_modules/pg-cloudflare/esm/**",
+    ],
+  },
 };
 
 const withNextIntl = createNextIntlPlugin("./src/core/i18n/request.ts");
